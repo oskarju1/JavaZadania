1ceb883d192fb5bc0f5e34db1bf0d1369225fca7
Oskar <wesoly.oski@interia.pl>
2021-04-09 16:38:23 +0200

    Zadanie 09.04

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.time.LocalDate;
 
 public class Human {
     public String firstName;
@@ -6,4 +7,29 @@ public class Human {
     public String phone;
     protected Animal pet;
     protected Car auto;
+    private Double salary;
+
+    public Human(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.salary = 23892183.00;
+    }
+
+    public Double getSalary() {
+        System.out.println(java.time.LocalDate.now());
+        System.out.println(this.salary);
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary > 0) {
+            System.out.println("Dane zostały wysłane do systemu księgowego");
+            System.out.println("Nalezy koniecznie odebrać aneks do umowy od Pani Hani z kadr");
+            System.out.println("ZUS i US już o tobie wiedzą nie ukrywaj dochodu");
+            this.salary = salary;
+        } else {
+            System.out.println("Nie mozna przypisac ujemnej wartosci wynagrodzenia");
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         Animal dog = new Animal("dog");
         dog.feed();
         dog.walk();
@@ -13,13 +12,12 @@ public class Main {
         dog.walk();
         dog.walk();
 
-
-        Human me = new Human();
+        Human me = new Human("Oskar", "Jurgielaniec", "593292322");
         me.firstName = "Oskar";
         me.lastName = "Jurgielaniec";
         me.pet = dog;
         me.auto = new Car("VW", "Golf");
-
-
+        me.getSalary();
+        me.setSalary(2313.00);
     }
 }
